d3a2d8ab5169c170cb2d8fba53fbf872e2b7aa9d
pax-n <paxton_nguyen@hotmail.com>
2022-03-10 20:04:38 -0500

    feature/user-auth

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -22,8 +22,7 @@ class Admin::CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(
-      :name,
-      :description,
+      :name
     )
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   
   get '/about', to: 'about#show'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -18,6 +25,9 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
+
+
+  
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
