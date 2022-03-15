6082a027f2a9b4c1c453dc73d48fd31799a07796
pax-n <paxton_nguyen@hotmail.com>
2022-03-14 21:00:56 -0400

    eature/user-specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :password, length: { minimum: 5 }
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :name, presence: true
+
+  #use self because it is a class method being called by User class
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+
+    if user && user.authenticate(password) 
+      return user
+    else 
+      return nil
+    end
+  end
 end
